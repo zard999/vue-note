974a0504507d8992a6d00822b01356498a80c4c8
Forever <1874861374@qq.com>
2021-11-17 17:58:31 +0800

    add: day06/04.单文件组件

---
+++ day06/04.单文件组件.vue
@@ -0,0 +1,17 @@
+<template></template>
+
+<script>
+export default {
+    
+};
+/**
+ * 1. 一个vue单文件组件就是指的是以.vue结尾的文件
+ * 2. 此文件有三部分组成
+ *      结构    html标签
+ *      样式    css样式
+ *      行为    js逻辑
+ */
+</script>
+
+<style>
+</style>
