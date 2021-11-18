0f1bdfd0b3555ae1ff90251e529bb50a6e5afbcd
Forever <1874861374@qq.com>
2021-11-18 14:31:10 +0800

    add: vuex-dd/02.手写min版vuex（state+mutation+getter）

---
+++ vuex-dd/02.手写min版vuex（state+mutation+getter）/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    {{ count }}
+    <br />
+    {{ $store.getters.doubleCount }}
+    <button @click="add">add</button>
+    <!-- <button @click="asyncAdd">async add</button> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  computed: {
+    count() {
+      return this.$store.state.count;
+    },
+  },
+  methods: {
+    add() {
+      // commit相当于执行了mutation里面的increment方法
+      this.$store.commit("increment", 2);
+    },
+
+    // asyncAdd() {
+    //   this.$store.dispatch("increment");
+    // },
+  },
+};
+</script>
+
+<style>
+</style>
