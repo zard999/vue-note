f1bd968f4c2ac71884c5d17c7206518ba8d7cf4c
Forever <1874861374@qq.com>
2021-11-19 08:25:11 +0800

    add vue-cl/01.简单留言板

---
+++ vue-cl/src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="app">
+    <Header></Header>
+    <div class="container">
+      <AddMessage @add="add"></AddMessage>
+      <CommentBack :commentList="commentList" :delItem="delItem"></CommentBack>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header/Header.vue";
+import AddMessage from "./components/AddMessage/AddMessage.vue";
+import CommentBack from "./components/CommentBack/CommentBack.vue";
+import "./App.css";
+export default {
+  name: "App",
+  components: {
+    Header,
+    AddMessage,
+    CommentBack,
+  },
+  data() {
+    return {
+      commentList: [
+        {
+          id: 0,
+          user: "zyh",
+          content: "React真不错！",
+        },
+        {
+          id: 1,
+          user: "cqh",
+          content: "Redux！",
+        },
+        {
+          id: 2,
+          user: "wyf",
+          content: "Hooks真不错！",
+        },
+      ],
+    };
+  },
+
+  methods: {
+    delItem(id) {
+      this.commentList = this.commentList.filter((item) => item.id !== id);
+    },
+
+    add(username, commentConent) {
+      this.commentList.unshift({
+        id: Date.now(),
+        user: username,
+        conent: commentConent,
+      });
+    },
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ vue-cl/vue.config.js
@@ -0,0 +1,3 @@
+module.export = {
+  lintOnSave: false, // 修改成false 就是不检查了(变量定义不使用就不会报错)
+};
