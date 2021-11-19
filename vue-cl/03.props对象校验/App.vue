0d89d3ef6629c77862b687e810f53f3761159fe9
Forever <1874861374@qq.com>
2021-11-19 11:46:41 +0800

    add: vue-cl//03.props对象校验

---
+++ vue-cl/03.props对象校验/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <input type="text" />
+    <Hello :msg="msg"></Hello>
+  </div>
+</template>
+
+<script>
+import Hello from "./components/Hello.vue";
+export default {
+  components: {
+    Hello,
+  },
+  data() {
+    return {
+      msg: -1,
+    };
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ vue-cl/03.props对象校验/components/Hello.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h2>{{ msg }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Hello",
+  props: {
+    msg: {
+      type: Number, // 指定数据类型 如果类型不符合会报错
+      require: true,
+      default: "hhh",
+      validator: (value) => { //值校验
+        return value > 0;
+      },
+    },
+  },
+};
+</script>
+
+<style>
+</style>
